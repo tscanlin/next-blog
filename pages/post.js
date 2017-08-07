f782e020d90039b7bc20779b0c7c3dbac5eeeab6
Tim Scanlin <tscanlin@gmail.com>
2017-08-06 23:39:14 -0700

    work on implementing custom paths

--- pages/post.js
+++ pages/post.js
@@ -7,10 +7,15 @@ import CONFIG from '../content/index.json'
 import SUMMARY_JSON from '../content/summary.json'
 
 function Index(props) {
-  let pageJson
+  let pageJson = {}
   if (props.url.query) {
-    pageJson = require(`../content${props.url.query.fullUrl}.json`)
+    if (props.url.query.fullUrl) {
+      pageJson = require(`../content${props.url.query.fullUrl}.json`)
+    } else if (props.url.query.filePath) {
+      pageJson = require(`../${props.url.query.filePath}`)
+    }
   }
+
   return (
     <div>
       <style jsx global>{`
