e1ecdda7c28fff6703d7ee6f9cfdd404a5757525
Tim Scanlin <tscanlin@gmail.com>
2018-09-02 17:58:32 -0700

    update next

--- pages/post.js
+++ pages/post.js
@@ -8,11 +8,12 @@ import SUMMARY_JSON from '../content/summary.json'
 
 function Index(props) {
   let pageJson = {}
+  console.log(props)
   if (props.url.query) {
     if (props.url.query.fullUrl) {
       pageJson = require(`../content${props.url.query.fullUrl}.json`)
     } else if (props.url.query.filePath) {
-      pageJson = require(`../${props.url.query.filePath}`)
+      // pageJson = require(`../${props.url.query.filePath}`)
     }
   }
 
